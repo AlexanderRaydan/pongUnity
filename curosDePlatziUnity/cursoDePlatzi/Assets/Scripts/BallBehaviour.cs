229fcd3e8d2b8c9d4d20d3f953510524bc0161ad
AlexanderRaydan <alexraydancode@gmail.com>
2019-05-29 15:23:28 -0300

    .

---
+++ curosDePlatziUnity/cursoDePlatzi/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+
+	public Transform paddle ;
+
+	bool gameStart = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!gameStart){
+
+		float posDiferen = paddle.position.x - transform.position.x; // la diferencia entre las posiciones de el paddle y la polota , para que la pelota aparezca justo adelante
+
+		transform.position = new Vector3( paddle.position.x - posDiferen , paddle.position.y , 0 );
+		}
+
+		if(Input.GetMouseButtonDown(0)) // el cero es el click izquierdo (el primer comando de la funcion) cuando le den click el juego comienza y la pelota deja de segir a el paddle
+			gameStart = true;
+	}
+}
---
+++ curosDePlatziUnity/cursoDePlatzi/Assets/Scripts/PaddleMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Debug.Log(Input.mousePosition);
+
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+
+		transform.position = new Vector3 ( transform.position.x , Mathf.Clamp(mousePos.y , -3.8f , 3.8f ) , 0 );
+
+		// mover el objeto con la posicion del mouse , el math.clamp es para que el objeto o pase la pantalla 
+		
+	}
+}
